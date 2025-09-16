f000d39dea0a4d22b69f33c6c8f9adc6c42cbc9f
M1nhng <nguyenchuminh223@gmail.com>
2025-09-16 10:44:30 +0700

    create

---
+++ Create.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Create {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame();
+        jf.setTitle("Login");   
+        jf.setSize(600,400);
+        jf.setLocation(650, 300);   
+        jf.setVisible(true);    
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
