e584e6491486e827c1f2bf60951a37abecd4fab0
M1nhng <nguyenchuminh223@gmail.com>
2025-09-16 09:36:33 +0700

    create

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        JFrame jf = new JFrame();
+        jf.setTitle("Hotel Management");
+        jf.setSize(600, 400);
+        jf.setLocation(650, 300);
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
