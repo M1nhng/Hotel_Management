de908f3fc92db6cef161656138dd1e7e055c75dc
M1nhng <nguyenchuminh223@gmail.com>
2025-09-18 14:54:36 +0700

    Create

--- src/App.java
+++ src/App.java
@@ -1,10 +1,26 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        JFrame jf = new JFrame();
-        jf.setSize(600, 400);
-        jf.setVisible(true);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+public class App extends JFrame {
+	public App() {
+		this.setSize(1400, 850);
+		this.setTitle("Hotel Management");
+		this.setLocationRelativeTo(null);
+		
+		GridLayout flowLayout = new GridLayout(1, 1, 4, 4);
+		this.setLayout(flowLayout);
+		
+		JButton jButton1 = new JButton("Đặt Phòng");
+		JButton jButton2 = new JButton("Trả Phòng");
+		this.add(jButton1);
+		this.add(jButton2);
+		
+		
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+    public static void main(String[] args) {
+        new App();
     }
 }
