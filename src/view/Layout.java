6ea840260be2e9d5a9afa06b5d375df944e18ec6
M1nhng <nguyenchuminh223@gmail.com>
2025-09-19 17:14:54 +0700

    Khung

---
+++ src/view/Layout.java
@@ -0,0 +1,31 @@
+package view;
+
+import javax.swing.*;
+
+public class Layout extends JFrame {
+    private JMenuBar menuBar;
+    private JPanel mainPanel;
+
+    public Layout() {
+        
+    }
+
+}
+
+public class KhachHangView extends JPanel {
+    private JTable tableKhachHang;
+    private JButton btnThem, btnSua, btnXoa;
+
+    public KhachHangView() {
+        
+    }
+
+public class PhongView extends JPanel {
+    private JTable tablePhong;
+    private JButton btnThem, btnSua, btnXoa;
+
+    public PhongView() {
+    
+    }
+
+}
