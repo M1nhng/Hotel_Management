aa3da3d2ec0f7b6f0a4ab632ff4bcd18e051a758
M1nhng <nguyenchuminh223@gmail.com>
2025-09-19 10:21:34 +0700

    Update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Hotel_Management
+Assigment: https://docs.google.com/document/d/1HDP6sxMD3U_8rL1wpECVtyhApcVY33IQFW-5Mb87Rts/edit?tab=t.0
--- src/view/Layout.java
+++ src/view/Layout.java
@@ -10,13 +10,27 @@ public class Layout extends JFrame {
 		
 		JPanel jPanel_button = new JPanel();
 		jPanel_button.setLayout(new FlowLayout());
-		JButton jButton1 = new JButton("Đặt Phòng");
-		JButton jButton2 = new JButton("Trả Phòng");
-		jPanel_button.add(jButton1);
-		jPanel_button.add(jButton2);
+		JButton jButton_datPhong = new JButton("Đặt Phòng");
+		JButton jButton_traPhong = new JButton("Trả Phòng");
+		JButton jButton_thongTin = new JButton("Thông Tin");
+		JButton jButton_danhSachPhong = new JButton("Danh Sách Phòng");
+		
+		jPanel_button.add(jButton_datPhong);
+		jPanel_button.add(jButton_traPhong);
+		jPanel_button.add(jButton_thongTin);
+		jPanel_button.add(jButton_danhSachPhong);
+		
+		JTextField jTextField = new JTextField(30);
+//		jTextField.setLayout(new GridLayout(3, 1));
+		
+		JPanel jPanel_text1 = new JPanel();
+//		JPanel jPanel_text2 = new JPanel();
+		jPanel_text1.add(jTextField, BorderLayout.WEST);
+//		jPanel_text2.add(jTextField, BorderLayout.WEST);
 		
 		this.add(jPanel_button, BorderLayout.WEST);
-		
+		this.add(jPanel_text1, BorderLayout.EAST);
+//		this.add(jPanel_text2, BorderLayout.EAST);
 		
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
